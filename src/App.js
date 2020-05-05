f685e4e72f40d0a65e8990cfe705cc004b157db5
Bhagirthi Jhamb <bhagirthi.jhamb@gmail.com>
2020-05-05 17:39:03 -0400

    React Router implemented

--- src/App.js
+++ src/App.js
@@ -1,19 +1,59 @@
 import React, { Component } from 'react';
+import {
+  BrowserRouter as Router,
+  Route,
+  Link,
+  NavLink,
+} from "react-router-dom";
 import './App.scss';
-import Guest from './components/Guest';
+import CreateGuest from './components/CreateGuest';
 
 class App extends Component {
   constructor(){
     super()
     this.state = {
-      
+
     }
   }
   render(){
     return (
-      <div className="App">
-        <Guest />
-      </div>
+      <Router>
+        <div className="App">
+          {/* Navigation */}
+          <nav>
+            <NavLink
+              exact
+              className="link"
+              activeClassName=""
+              to="/"
+            >
+              Home
+            </NavLink>
+            <NavLink
+              exact
+              className="link"
+              activeClassName=""
+              to="/createGuest"
+            >
+              Create Guest
+            </NavLink>
+            <NavLink className="link" activeClassName="" to="/">
+              Create Party
+            </NavLink>
+            <NavLink className="link" activeClassName="" to="/contact">
+              View Party
+            </NavLink>
+            <NavLink className="link" activeClassName="" to="/contact">
+              View Guests
+            </NavLink>
+          </nav>
+          {/* Routes */}
+          <Route path="/createGuest" component={CreateGuest} />
+          {/* <Route path="/about" component={CreateParty} />
+          <Route exact path="/" component={ViewGuest} />
+          <Route exact path="/" component={ViewParty} /> */}
+        </div>
+      </Router>
     );
   }  
 }
