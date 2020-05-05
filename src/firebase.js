bb37dad6bb2abb497fd59f6f8bbc3387623a641f
mseneoudom <matt_sen@live.ca>
2020-05-05 16:57:06 -0400

    added firebase

---
+++ src/firebase.js
@@ -0,0 +1,15 @@
+import firebase from 'firebase/app';
+import 'firebase/database'
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAeYLavcsXAu2qyS3m3sl80bPDNeCrUqlk",
+    authDomain: "bemp-app-ed66c.firebaseapp.com",
+    databaseURL: "https://bemp-app-ed66c.firebaseio.com",
+    projectId: "bemp-app-ed66c",
+    storageBucket: "bemp-app-ed66c.appspot.com",
+    messagingSenderId: "929043957791",
+    appId: "1:929043957791:web:052c46f8f3643e8f5fad2a"
+};
+
+  // Initialize Firebase
+firebase.initializeApp(firebaseConfig);
