fc1aa4a2d25312768981eef5f53a22cdf12b0a40
mseneoudom <matt_sen@live.ca>
2020-05-06 14:10:32 -0400

    added component ViewGGuest and link .. will use branch going forward

--- src/App.js
+++ src/App.js
@@ -7,6 +7,9 @@ import {
 } from "react-router-dom";
 import './App.scss';
 import CreateGuest from './components/CreateGuest';
+import ViewGuests from './components/ViewGuests'
+// import FocusGuest from './components/FocusGuest';
+
 
 class App extends Component {
   constructor(){
@@ -40,18 +43,19 @@ class App extends Component {
             <NavLink className="link" activeClassName="" to="/">
               Create Party
             </NavLink>
-            <NavLink className="link" activeClassName="" to="/contact">
+            <NavLink className="link" activeClassName="" to="/parties">
               View Party
             </NavLink>
-            <NavLink className="link" activeClassName="" to="/contact">
+            <NavLink className="link" activeClassName="" to="/guests">
               View Guests
             </NavLink>
           </nav>
           {/* Routes */}
           <Route path="/createGuest" component={CreateGuest} />
-          {/* <Route path="/about" component={CreateParty} />
-          <Route exact path="/" component={ViewGuest} />
-          <Route exact path="/" component={ViewParty} /> */}
+          {/* <Route path="/about" component={CreateParty} /> */}
+          <Route exact path="/guests" component={ViewGuests}/>
+          {/* <Route exact path="/guests:id" component={FocusGuest}/> */}
+          {/* <Route exact path="/" component={ViewParty} /> */}
         </div>
       </Router>
     );
