2f75c183fffd3938ed7a2297960b2fcd665ec39e
Bhagirthi Jhamb <bhagirthi.jhamb@gmail.com>
2020-05-06 12:08:37 -0400

    updating CreateGuest Component

--- src/components/CreateGuest.js
+++ src/components/CreateGuest.js
@@ -73,69 +73,132 @@ class CreateGuest extends Component {
               <br />
               <p></p>
 
-              <label>
-                <input
-                  type="checkbox"
-                  name="isVegan"
-                  onChange={this.handleChange}
-                  checked={this.state.isVegan}
-                />{" "}
-                Vegan?
-              </label>
-              <br />
+              <ul>
+                <li>
+                  <label>
+                    <input
+                      type="checkbox"
+                      name="isVegan"
+                      onChange={this.handleChange}
+                      checked={this.state.isVegan}
+                    />{" "}
+                    Vegan ?
+                  </label>
+                </li>
 
-              <label>
-                <input
-                  type="checkbox"
-                  name="isKosher"
-                  onChange={this.handleChange}
-                  checked={this.state.isKosher}
-                />{" "}
-                Kosher?
-              </label>
-              <br />
+                <li>
+                  <label>
+                    <input
+                      type="checkbox"
+                      name="isVegetarian"
+                      onChange={this.handleChange}
+                      checked={this.state.isVegetarian}
+                    />{" "}
+                    Vegetarian ?
+                  </label>
+                </li>
+                <li>
+                  <label>
+                    <input
+                      type="checkbox"
+                      name="isPaleo"
+                      onChange={this.handleChange}
+                      checked={this.state.isPaleo}
+                    />{" "}
+                    Paleo ?
+                  </label>
+                </li>
+                <li>
+                  <label>
+                    <input
+                      type="checkbox"
+                      name="isDairyFree"
+                      onChange={this.handleChange}
+                      checked={this.state.isDairyFree}
+                    />{" "}
+                    Dairy free?
+                  </label>
+                </li>
+                <li>
+                  <label>
+                    <input
+                      type="checkbox"
+                      name="isGlutenFree"
+                      onChange={this.handleChange}
+                      checked={this.state.isGlutenFree}
+                    />{" "}
+                    Gluten free?
+                  </label>
+                </li>
+                <li>
+                  <label>
+                    <input
+                      type="checkbox"
+                      name="isWheatFree"
+                      onChange={this.handleChange}
+                      checked={this.state.isWheatFree}
+                    />{" "}
+                    Wheat free?
+                  </label>
+                </li>
+                <li>
+                  <label>
+                    <input
+                      type="checkbox"
+                      name="isWheatFree"
+                      onChange={this.handleChange}
+                      checked={this.state.isWheatFree}
+                    />{" "}
+                    Wheat free?
+                  </label>
+                </li>
+                <li>
+                  <label>
+                    <input
+                      type="checkbox"
+                      name="isWheatFree"
+                      onChange={this.handleChange}
+                      checked={this.state.isWheatFree}
+                    />{" "}
+                    Wheat free?
+                  </label>
+                </li>
+                <li>
+                  <label>
+                    <input
+                      type="checkbox"
+                      name="isWheatFree"
+                      onChange={this.handleChange}
+                      checked={this.state.isWheatFree}
+                    />{" "}
+                    Wheat free?
+                  </label>
+                </li>
+                <li>
+                  <label>
+                    <input
+                      type="checkbox"
+                      name="isWheatFree"
+                      onChange={this.handleChange}
+                      checked={this.state.isWheatFree}
+                    />{" "}
+                    Wheat free?
+                  </label>
+                </li>
+              </ul>
 
-              <label>
-                <input
-                  type="checkbox"
-                  name="isPeanutFree"
-                  onChange={this.handleChange}
-                  checked={this.state.isPeanutFree}
-                />{" "}
-                Peanut free?
-              </label>
-              <br />
-
-              <label>
-                <input
-                  type="checkbox"
-                  name="isLactoseFree"
-                  onChange={this.handleChange}
-                  checked={this.state.isLactoseFree}
-                />{" "}
-                Lactose free?
-              </label>
-              <br />
-              <label>
-                <input
-                  type="checkbox"
-                  name="isGlutenFree"
-                  onChange={this.handleChange}
-                  checked={this.state.isGlutenFree}
-                />{" "}
-                Gluten free?
-              </label>
-              <br />
-              <p></p>
+              {/* Text Area */}
+              <h3>Something you would like us to know</h3>
               <textarea
-                value={"Some else you would like to mention"}
+                value={"Something else you would like to mention"}
                 onChange={this.handleChange}
               />
               <br />
               <button>Submit</button>
             </form>
 
-            <div>
+            <h2>Guest Information</h2>
+            <div className="guestInformation">
               <p>Your name: {this.state.name}</p>
               <p>Your email id: {this.state.email}</p>
               <p>
@@ -144,8 +207,22 @@ class CreateGuest extends Component {
               </p>
               <p>
                 Your dietary restictions:
-                Vegan: 
-                
+                <ul>
+                  <li>Vegan: {this.state.isVegan ? "Yes" : "No"}</li>
+                  <li>Vegetatian: {this.state.isVegetarian ? "Yes" : "No"}</li>
+                  <li>Paleo: {this.state.isPaleo ? "Yes" : "No"}</li>
+                  <li>Dairy Free: {this.state.isDairyFree ? "Yes" : "No"}</li>
+                  <li>Gluten Free: {this.state.isGlutenFree ? "Yes" : "No"}</li>
+                  <li>Wheat Free: {this.state.isWheatFree ? "Yes" : "No"}</li>
+                  <li>Fat Free: {this.state.isVegan ? "Yes" : "No"}</li>
+                  <li>Low Sugar: {this.state.isVegan ? "Yes" : "No"}</li>
+                  <li>Egg Free: {this.state.isVegan ? "Yes" : "No"}</li>
+                  <li>Peanut Free: {this.state.isVegan ? "Yes" : "No"}</li>
+                  {/* <li>Tree Nut Free: {this.state.isVegan ? "Yes" : "No"}</li>
+                  <li>Soy Free: {this.state.isVegan ? "Yes" : "No"}</li>
+                  <li>Fish Free: {this.state.isVegan ? "Yes" : "No"}</li>
+                  <li>Shell Fish Free: {this.state.isVegan ? "Yes" : "No"}</li> */}
+                </ul>
                 {/* {this.state.isPetFriendly ? "pet friendly" : "not pet friendly"} */}
               </p>
             </div>
