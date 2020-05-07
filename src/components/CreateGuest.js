0bd6f842e87217f4c56f852b786ff2e09a8cdbb8
Bhagirthi Jhamb <bhagirthi.jhamb@gmail.com>
2020-05-07 17:32:17 -0400

    Guest Image upload functionality implemented

--- src/components/CreateGuest.js
+++ src/components/CreateGuest.js
@@ -1,15 +1,18 @@
 import React, { Component } from 'react';
 import firebase from '../firebase';
+import storage from '../firebase';
+
 
 class CreateGuest extends Component {
   constructor() {
     super();
     this.state = {
-      // selectedFile: null,
+      files: null,
+      url: '',
       name: "",
       email: "",
       isPetFriendly: false,
-      diet:'',
+      diet: "",
       // Dietary Restrictions
       isDairyFree: false,
       isEggFree: false,
@@ -23,15 +26,15 @@ class CreateGuest extends Component {
       isSulfiteFree: false,
       isTreeNutFree: false,
       isWheatFree: false,
-      allergies: []
+      allergies: [],
     };
   }
 
   populateAllergies = () => {
     const allergiesArray = [];
-    this.state.isDairyFree 
-      ? allergiesArray.push('dairy') 
-      : console.log('dont push');
+    this.state.isDairyFree
+      ? allergiesArray.push("dairy")
+      : console.log("dont push");
     this.state.isEggFree
       ? allergiesArray.push("egg")
       : console.log("dont push");
@@ -66,51 +69,80 @@ class CreateGuest extends Component {
       ? allergiesArray.push("shellfish")
       : console.log("dont push");
 
-      return allergiesArray;
-      
-    }
-    
-    handleChange = (e) => {
-      // make a copy of current state before you run setState
-      const { name, value, type, checked } = e.target;
-      type === "checkbox"
+    return allergiesArray;
+  };
+
+  handleChange = (e) => {
+    // make a copy of current state before you run setState
+    const { name, value, type, checked } = e.target;
+    type === "checkbox"
       ? this.setState({
-        [name]: checked,
-      })
+          [name]: checked,
+        })
       : this.setState({
-        [name]: value,
-      });        
-    };
-    
-    // handleImageChange = (e) => {
-      //   this.setState({
-  //     selectedFile: e.target.file,
-  //   });
-  // };
-  
+          [name]: value,
+        });
+  };
+
+  handleImageChange = (e) => {
+    console.log(e.target.files)
+    const files = e.target.files;
+    this.setState({
+      files: files
+    })
+  };
+
+  handleUploadImage = (e) => {
+    e.preventDefault();
+    const bucketName = 'images';
+    const file = this.state.files[0];
+    firebase
+      .storage()
+      .ref(`${bucketName}/${file.name}`)
+      .put(file)
+      .then(() => {
+        firebase
+          .storage()
+          .ref(`${bucketName}`)
+          .child(file.name)
+          .getDownloadURL()
+          .then((url) => {
+            console.log(url);
+            this.setState({
+              url: url,
+            });
+            document.getElementById('guestImg').src= url;
+          });
+      });
+  };
+
   handleSubmit = (e) => {
     e.preventDefault();
     const allergiesArray = this.populateAllergies();
-    console.log(allergiesArray);
-    this.setState({
-      allergies: allergiesArray,
-    }, () => {
-      const dbRef = firebase.database().ref(`/Guests/`);
-      dbRef.push({
-        // image: this.state.image,
-        name: this.state.name,
-        email: this.state.email,
-        petFriendly: this.state.isPetFriendly,
-        diet: this.state.diet,
-        allergies: this.state.allergies,
-      });
-    });
-    
+    this.handleUploadImage(e);
+    // console.log(allergiesArray);
+    this.setState(
+      {
+        allergies: allergiesArray,
+      },
+      () => {
+        const dbRef = firebase.database().ref(`/Guests/`);
+        dbRef.push({
+          url: this.state.url,
+          name: this.state.name,
+          email: this.state.email,
+          petFriendly: this.state.isPetFriendly,
+          diet: this.state.diet,
+          allergies: this.state.allergies,
+        });
+      }
+    );
+
   };
 
-  getChoice = (event) =>{
+  getChoice = (event) => {
     event.preventDefault();
-  }
+  };
 
   render() {
     return (
@@ -123,10 +155,10 @@ class CreateGuest extends Component {
           <input
             value={this.state.image}
             type="file"
-            id="img"
-            name="img"
-            accept="image/*"
-          ></input>
+            onChange={(e) => {
+            this.handleImageChange(e)
+            }}
+          />
           <br />
           <p></p>
           {/* Name input */}
@@ -359,18 +391,13 @@ class CreateGuest extends Component {
             </li>
           </ul>
 
-          {/* Text Area */}
-          {/* <h3>Something you would like us to know</h3>
-              <textarea
-                value={"Something else you would like to mention"}
-                onChange={this.handleChange}
-              /> */}
           <br />
           <button onClick={(e) => this.handleSubmit(e)}>Submit</button>
         </form>
 
         <h2>Guest Information</h2>
         <div className="guestInformation">
+          <img  id="guestImg" src="" alt=""/>
           <p>Your name: {this.state.name}</p>
           <p>Your email id: {this.state.email}</p>
           <p>
