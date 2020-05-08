b670ed018c4a5c4ac87a8fc0c6a65ff998238798
Emma <emmajanewalden@gmail.com>
2020-05-07 20:44:49 -0400

    added Home.js comp, added partials for variables with the colors and fonts styled header

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,7 +3,13 @@ import React from 'react';
 function Home() {
     return (
         <header>
-        <h1>Love in the Time of Allergies</h1>
+            <div className="wrapper headingText">
+            <h1>Love in the Time of Allergies</h1>
+            </div>
+            <div className="wrapper homeIntro">
+                <p>Make sure to account for your guest's dietary needs at your next party.
+                Create guest profiles with any allergies or diets, and then create your party with the guests attending. Search for recipes that will accomodate them and make your next party a great success.</p>
+            </div>
         </header>
     )
 }
