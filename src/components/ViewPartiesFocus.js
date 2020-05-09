d0c1b9588e36b3cd024ea4aa5debc46dfa642f64
mseneoudom <matt_sen@live.ca>
2020-05-09 16:52:17 -0400

    styling for view parties and view guests

--- src/components/ViewPartiesFocus.js
+++ src/components/ViewPartiesFocus.js
@@ -11,6 +11,8 @@ class ViewPartiesFocus extends Component{
                 photoURL:'',
                 dietList:[],
                 intoleranceList:[],
+                addedGuests:[],
+                recipes:[]
             }
         }
     }
@@ -36,27 +38,69 @@ class ViewPartiesFocus extends Component{
     }
 
     render(){
+        console.log(this.state.focusedParty);
+        
         return(
-            <section className="viewPartiesFocusSection">
-                <div className="imageContainer">
-                    <img src={this.state.focusedParty.photoURL} alt={this.state.focusedParty.partyDetails}/>
-                </div>
-                <h2>{this.state.focusedParty.partyName}</h2>
-                <p>{this.state.focusedParty.partyDetails}</p>
+            <section className="viewPartiesFocusSection wrapper">
+                <div className="profileCard">
+                    <div className="imageContainer">
+                        <img src={this.state.focusedParty.photoURL} alt={this.state.focusedParty.partyDetails}/>
+                    </div>
+                    <h2>{this.state.focusedParty.partyName}</h2>
+                    <p>{this.state.focusedParty.partyDetails}</p>
 
-                <div className="restrictionsList">
-                    <h3>Warning! Be careful of the following:</h3>
-                    <ul className="dietListFocusUL">
-                        {this.state.focusedParty.dietList.map((diet)=>{
-                            return <li className="dietFocusLI">{diet}</li>
-                        })}
-                    </ul>
+                    <div className="guestList">
+                        <h3>Guest List</h3>
+                        <ul className="guestListUL">
+                            {this.state.focusedParty.addedGuests.map((guest)=>{
+                                return <li className="guestLI"><p>{guest.name}</p></li>
+                            })}
+                        </ul>
+                    </div>
 
-                    <ul className="intoleranceListFocusUL">
-                        {this.state.focusedParty.intoleranceList.map((intolerance)=>{
-                            return <li className="intoleranceFocusLI">{intolerance}</li>
+                    <div className="restrictionsList">
+                        <h3>Warning! Be careful of the following:</h3>
+                        <ul className="dietListFocusUL">
+                            {this.state.focusedParty.dietList.map((diet)=>{
+                                return <li className="dietFocusLI"><p>{diet}</p></li>
+                            })}
+                        </ul>
+
+                        <ul className="intoleranceListFocusUL">
+                            {this.state.focusedParty.intoleranceList.map((intolerance)=>{
+                                return <li className="intoleranceFocusLI"><p>{intolerance}</p></li>
+                            })}
+                        </ul>
+                    </div>
+
+                    <section className="recipeGallerySection">
+                        <h3>Suggested Recipes</h3>
+                        <ul className="recipeGalleryUL">
+                        {this.state.focusedParty.recipes.map((recipeObj) => {
+                            return (
+                            <li className="recipeLI">
+                                <h4 className="recipeLink">
+                                <a
+                                    rel="noopener noreferrer"
+                                    target="_blank"
+                                    href={recipeObj.sourceUrl}
+                                >
+                                    {recipeObj.title}
+                                </a>
+                                </h4>
+                                <div className="imageContainer">
+                                <img className="recipeImg"
+                                src={`https://spoonacular.com/recipeImages/${
+                                    recipeObj.id
+                                }-${"480x360"}.${"jpg"}`}
+                                alt={recipeObj.title}
+                                />
+                                </div>
+                            </li>
+                            );
                         })}
-                    </ul>
+                        </ul>
+                    </section>
                 </div>
             </section>
         )
